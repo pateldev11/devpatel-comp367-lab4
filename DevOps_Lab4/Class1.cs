f6e046149bbbd618c1a96e47180367d9267e4e70
Devkumar Patel <dpat1072@my.centennialcollege.ca>
2025-04-05 14:51:20 -0400

    made lab4

---
+++ DevOps_Lab4/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevOps_Lab4
+{
+    public static class StringHelper
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
